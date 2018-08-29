288de1bbcf818f109b75550c6839fdf19c242c07
KwinstonRoberts <kyle.w.r@hotmail.com>
2018-08-28 20:15:45 -0400

    hello world test

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const app = require('express');
-
-app.post('/command/echo', (req, res) => {
-    res.send(200);
-    res.send({
-        "text": "Hello World."
-      });
-});
+const router = new app.Router();
+router.post('/command/echo', async(req, res) => {
+    try{
+        const slackReqObj = req.body;
+        const response = {
+          response_type: 'ephemeral',
+          text: 'Hello World:slightly_smiling_face:',
+        }
+        return res.json(response);
+    } catch (err) {
+        log.error(err);
+        return res.status(500).send('Something blew up. We\'re looking into it.');
+      }
+    });
+server.listen(port);
