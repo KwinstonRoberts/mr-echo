1cec8b6276c233833cc05694246e71a1f3a3e711
KwinstonRoberts <kyle.w.r@hotmail.com>
2018-08-28 20:01:59 -0400

    hello world test

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const express = require('express');
+const app = require('express');
 
 app.post('/command/echo', (req, res) => {
     res.send(200);
     res.send({
-        "response_type": "in_channel",
         "text": "Hello World."
       });
 });
