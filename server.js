1f21bc2134b0a6fc259f9db81c76a62e7eac91db
KwinstonRoberts <kyle.w.r@hotmail.com>
2018-08-29 17:01:58 -0400

    fixing broken logs

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 const https = require('https');
 
 app.post('/command/echo', async(req, res) => {
-    console.log(req)
+    console.log(req);
     try{
         const slackReqObj = req.body;
-        console.log
+        console.log(slackReqObj);
         if(slackReqObj.item === "burndown"){
 
             var options = {
@@ -40,7 +40,7 @@ app.post('/command/echo', async(req, res) => {
         }
         return res.json(response);
     } catch (err) {
-        log.error(err);
+        console.error(err);
         return res.status(500).send('Something blew up. We\'re looking into it.');
       }
     });
@@ -48,40 +48,7 @@ app.post('/command/echo', async(req, res) => {
     app.post('/command/thread', async(req, res) => {
         console.log(req);
         try{
-            const slackReqObj = req.body;
-            console.log
-            if(slackReqObj.item === "burndown"){
-    
-                var options = {
-                    host: 'https://www.pivotaltracker.com',
-                    path: '/services/v5'
-                  };
-                var bodyChunks = [];
-                var burndown = http.get(options, function(res) {
-                    console.log('STATUS: ' + res.statusCode);
-                    console.log('HEADERS: ' + JSON.stringify(res.headers));
-                  
-                    // Buffer the body entirely for processing as a whole.
-                    res.on('data', function(chunk) {
-                      // You can process streamed parts here...
-                      bodyChunks.push(chunk);
-                    }).on('end', function() {
-                      var body = Buffer.concat(bodyChunks);
-                      console.log('BODY: ' + body);
-                      // ...and/or process the entire body here.
-                    })
-                  });
-                  
-                  burndown.on('error', function(e) {
-                    console.log('ERROR: ' + e.message);
-                  });
-    
-            }
-            const response = {
-              response_type: 'ephemeral',
-              text: 'Hello World:parrot:',
-            }
-            return res.json(response);
+           
         } catch (err) {
             console.error(err);
             return res.status(500).send('Something blew up. We\'re looking into it.');
