dc0e0de9c7627d3371db3e18853c487609088016
KwinstonRoberts <kyle.w.r@hotmail.com>
2018-08-29 16:31:19 -0400

    fixed typo

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 const https = require('https');
 
 app.post('/command/echo', async(req, res) => {
-    res.send(200);
     console.log(req)
     try{
         const slackReqObj = req.body;
@@ -46,8 +45,8 @@ app.post('/command/echo', async(req, res) => {
       }
     });
 
-    app.post('/command/echo', async(req, res) => {
-        console.log(req)
+    app.post('/command/thread', async(req, res) => {
+        console.log(req);
         try{
             const slackReqObj = req.body;
             console.log
@@ -84,7 +83,7 @@ app.post('/command/echo', async(req, res) => {
             }
             return res.json(response);
         } catch (err) {
-            log.error(err);
+            console.error(err);
             return res.status(500).send('Something blew up. We\'re looking into it.');
           }
         });
