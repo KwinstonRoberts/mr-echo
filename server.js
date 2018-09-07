dab33fbcaf1f38399f9fd0c9f528379caecee8cc
Kyle Roberts <kylewr@tm7.tech>
2018-09-07 14:08:34 -0400

    fixed response body

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post('/command/echo', async(req, res) => {
             });
         });
       
-        return res.status(200).json(res);
+        return res.status(200).json(response);
       } catch (err) {
         console.error(err);
         return res.status(500).send('Something blew up. We\'re looking into it.');
