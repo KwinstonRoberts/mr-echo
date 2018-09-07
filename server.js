80cd1f02b0bd5ba345815552b66128110fd5826a
Kyle Roberts <kylewr@tm7.tech>
2018-09-07 11:45:26 -0400

    fix response code

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.post('/command/echo', async(req, res) => {
         console.log(slackReqObj);
         var url = 'https://www.pivotaltracker.com/services/v5/projects/2182748/iterations?token=' + process.env.pivitolToken;
         var bodyChunks = [];
+        var pivitRes = [];
         var tickets = https.get(url, function(res) {
             console.log('STATUS: ' + res.statusCode);
             console.log('HEADERS: ' + JSON.stringify(res.headers));
@@ -17,18 +18,17 @@ app.post('/command/echo', async(req, res) => {
               // You can process streamed parts here...
               bodyChunks.push(chunk);
             }).on('end', function() {
-              var body = Buffer.concat(bodyChunks);
+              pivotRes = Buffer.concat(bodyChunks);
               //console.log('BODY: ' + body);
-              return res.json;
             }).on('error', function(e) {
                 console.log('ERROR: ' + e.message);
             });
         });
           
-        console.log(tickets.body);
+        console.log(pivotRes);
         const response = {
           response_type: 'ephemeral',
-          text: 'Hello World:parrot:\n there are ' + tickets.body + ' stories in pivitol tracker'
+          text: 'Hello World:parrot:\n there are ' + pivotRes + ' stories in pivitol tracker'
         }
         return res.status(200).json(response);
     } catch (err) {
