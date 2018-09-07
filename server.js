1915580054884a27dbcd1f4c66065873ecfc40be
Kyle Roberts <kylewr@tm7.tech>
2018-09-07 11:09:49 -0400

    gitignore fix

--- server.js
+++ server.js
@@ -6,30 +6,26 @@ app.post('/command/echo', async(req, res) => {
     try{
         const slackReqObj = req.body;
         console.log(slackReqObj);
-
-            var options = {
-                host: 'https://www.pivotaltracker.com',
-                path: '/services/v5/projects/2182748/iterations?token=' + process.env.pivitolToken
-              };
-            var bodyChunks = [];
-            var tickets = https.get(options, function(res) {
-                console.log('STATUS: ' + res.statusCode);
-                console.log('HEADERS: ' + JSON.stringify(res.headers));
-              
-                // Buffer the body entirely for processing as a whole.
-                res.on('data', function(chunk) {
-                  // You can process streamed parts here...
-                  bodyChunks.push(chunk);
-                }).on('end', function() {
-                  var body = Buffer.concat(bodyChunks);
-                  console.log('BODY: ' + body);
-                  // ...and/or process the entire body here.
-                }).on('error', function(e) {
-                    console.log('ERROR: ' + e.message);
-                });
+        var url = 'https://www.pivotaltracker.com/services/v5/projects/2182748/iterations?token=' + process.env.pivitolToken;
+        var bodyChunks = [];
+        var tickets = https.get(url, function(res) {
+            console.log('STATUS: ' + res.statusCode);
+            console.log('HEADERS: ' + JSON.stringify(res.headers));
+          
+            // Buffer the body entirely for processing as a whole.
+            res.on('data', function(chunk) {
+              // You can process streamed parts here...
+              bodyChunks.push(chunk);
+            }).on('end', function() {
+              var body = Buffer.concat(bodyChunks);
+              console.log('BODY: ' + body);
+              // ...and/or process the entire body here.
+            }).on('error', function(e) {
+                console.log('ERROR: ' + e.message);
             });
-              
-        
+        });
+          
+    
         const response = {
           response_type: 'ephemeral',
           text: 'Hello World:parrot:\n' + bodyChunks,
