09e784ea2db75582debeab26f13a9899d2e8d0ed
Kyle Roberts <kylewr@tm7.tech>
2018-09-13 11:40:19 -0400

    fixed arg check, format document

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser'); 
+const bodyParser = require('body-parser');
 const https = require('https');
 var MongoClient = require('mongodb').MongoClient;
 var mongoUrl = "mongodb://heroku_n0503mt5:hnl3j1m3olr852mmc4br24ceti@ds137812.mlab.com:37812/heroku_n0503mt5";
@@ -16,93 +16,94 @@ var text_args;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/command/echo', async(req, res) => {
-    try{
-        const slackReqObj = req.body;
-        console.log(slackReqObj);
-        var url = 'https://www.pivotaltracker.com/services/v5/projects/2182748/iterations?token=' + process.env.pivitolToken;
-        var bodyChunks = [];
-        var pivitRes = [];
-        https.get(url, function(res) {
-            console.log('STATUS: ' + res.statusCode);
-            console.log('HEADERS: ' + JSON.stringify(res.headers));
-          
-            // Buffer the body entirely for processing as a whole.
-            res.on('data', function(chunk) {
-              // You can process streamed parts here...
-              bodyChunks.push(chunk);
-            }).on('end', function() {
-              pivotRes = Buffer.concat(bodyChunks);
-              //console.log('BODY: ' + body);
-            }).on('error', function(e) {
-                console.log('ERROR: ' + e.message);
-            });
-        });
-          
-        console.log(pivotRes);
-        const response = {
-          response_type: 'ephemeral',
-          text: 'There are ' + pivotRes[pivotRes.length-1] + ' stories in pivitol tracker'
-        }
-        return res.status(200).json(response);
-    } catch (err) {
-        console.error(err);
-        return res.status(500).send('Something blew up. We\'re looking into it.');
-      }
+app.post('/command/echo', async (req, res) => {
+  try {
+    const slackReqObj = req.body;
+    console.log(slackReqObj);
+    var url = 'https://www.pivotaltracker.com/services/v5/projects/2182748/iterations?token=' + process.env.pivitolToken;
+    var bodyChunks = [];
+    var pivitRes = [];
+    https.get(url, function (res) {
+      console.log('STATUS: ' + res.statusCode);
+      console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+      // Buffer the body entirely for processing as a whole.
+      res.on('data', function (chunk) {
+        // You can process streamed parts here...
+        bodyChunks.push(chunk);
+      }).on('end', function () {
+        pivotRes = Buffer.concat(bodyChunks);
+        //console.log('BODY: ' + body);
+      }).on('error', function (e) {
+        console.log('ERROR: ' + e.message);
+      });
     });
 
-    app.post('/command/house', (req, res) => {
-      try{
-       text_args=null;
-        var housePromise = new Promise(function(resolve,reject){
-          MongoClient.connect(mongoUrl, function(err, db) {
-            var houseString = ''; 
-            if (err) throw err;
-            var dbo = db.db("heroku_n0503mt5");
-            if(req.body.text){
-              text_args = req.body.text.split('-');
-              console.log(text_args[0]);
-              let changePoints = parseInt(text_args[1].split(' ')[1]);
-              console.log(changePoints,text_args[0]);
-              if(changePoints[0] == '-a'){
-              dbo.collection("houses").updateOne({"name":text_args[0].trim()},{$inc:{'points':changePoints}});
-              }else if( changePoints[0] === '-s'){
-                dbo.collection("houses").updateOne({"name":text_args[0].trim()},{$inc:{'points':changePoints-(changepoints*2)}});
-              }
-              resolve(text_args);
-            }else{
-            dbo.collection("houses").find({}).toArray(function(err, result) {
-              result.forEach((house)=>{
-                 houseString += stickers[house.name] + house.name + ': ' + house.points + ' points\n'
-                
-              });
-             
-              if (err) throw err;
-              console.log(result,houseString);
-              db.close();
-              resolve(houseString);
+    console.log(pivotRes);
+    const response = {
+      response_type: 'ephemeral',
+      text: 'There are ' + pivotRes[pivotRes.length - 1] + ' stories in pivitol tracker'
+    }
+    return res.status(200).json(response);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('Something blew up. We\'re looking into it.');
+  }
+});
+
+app.post('/command/house', (req, res) => {
+  try {
+    text_args = null;
+    var housePromise = new Promise(function (resolve, reject) {
+      MongoClient.connect(mongoUrl, function (err, db) {
+        var houseString = '';
+        if (err) throw err;
+        var dbo = db.db("heroku_n0503mt5");
+        text_args = req.body.text.split('-');
+        console.log(text_args[0]);
+        let changePoints = parseInt(text_args[1].split(' ')[1]);
+        console.log(changePoints, text_args[0]);
+        if (changePoints[0] == '-a') {
+          dbo.collection("houses").updateOne({ "name": text_args[0].trim() }, { $inc: { 'points': changePoints } });
+          resolve(text_args);
+        } else if (changePoints[0] === '-s') {
+          dbo.collection("houses").updateOne({ "name": text_args[0].trim() }, { $inc: { 'points': changePoints - (changepoints * 2) } });
+          resolve(text_args);
+        } else {
+
+
+          dbo.collection("houses").find({}).toArray(function (err, result) {
+            result.forEach((house) => {
+              houseString += stickers[house.name] + house.name + ': ' + house.points + ' points\n'
+
             });
-          }
+
+            if (err) throw err;
+            console.log(result, houseString);
+            db.close();
+            resolve([houseString, result]);
           });
-        
-        });
-        housePromise.then((result)=>{
-          if(text_args && text_args.constructor === Array && (text_args[1]==="a"||text_args === "s")){
-            response = {
-              response_type: result[1] === 'p'? 'in_channel':'ephemeral',
-              text: 'points have been modified'
-            }
-          }else{
-          response = {
-            response_type: req.body.text && text_args[1] === 'p'? 'in_channel':'ephemeral',
-            text: ':parrot:House Tournament:parrot:\n-------------------------------\n' + result
-          }
         }
-          return res.json(response);
-        });
-      } catch (err) {
-        console.error(err);
-        return res.status(500).send('Something blew up. We\'re looking into it.');
-      }
+      });
+
     });
+    housePromise.then((result) => {
+      if (text_args && text_args.constructor === Array && (text_args[1] === "a" || text_args === "s")) {
+        response = {
+          response_type: result[1] === 'p' ? 'in_channel' : 'ephemeral',
+          text: 'points have been modified'
+        }
+      } else {
+        response = {
+          response_type: req.body.text && text_args[1] === 'p' ? 'in_channel' : 'ephemeral',
+          text: ':parrot:House Tournament:parrot:\n-------------------------------\n' + result
+        }
+      }
+      return res.json(response);
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send('Something blew up. We\'re looking into it.');
+  }
+});
 app.listen(process.env.PORT || 3000);
